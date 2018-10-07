9fefc8cc80155938136ef6d178f0248bba72d9df
Leo Liu <karta1041337@gmail.com>
2018-10-07 15:59:10 +0800

    Add saveas

---
+++ saveas_windows.go
@@ -0,0 +1,96 @@
+package dialog
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"leoliu.io/execute"
+	"leoliu.io/logger"
+)
+
+// SaveAs create Save As dialog in the background
+func SaveAs(filter string, initDir string) (path string, err error) {
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"filter": filter,
+				"path":   path,
+			}),
+		).Debugln("Create save as dialog . . .")
+	}
+
+	cmd, err := BgSaveAs(filter, initDir)
+	if err != nil {
+		if intLog {
+			intLogger.WithFields(logger.DebugInfo(1, logrus.Fields{})).
+				WithError(err).Errorln("Cannot create message box . . .")
+		}
+		return
+	}
+	cmd.Wait()
+	path = cmd.Strout()
+
+	if path == "" {
+		return "", errors.New("User canceled")
+	}
+
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"path": path,
+			}),
+		).Debugln("Create save as dialog")
+	}
+	return
+}
+
+// BgSaveAs create Save As dialog in the background
+func BgSaveAs(filter string, initDir string) (cmd *execute.Cmd, err error) {
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"filter":            filter,
+				"initial_directory": initDir,
+			}),
+		).Debugln("Create save as dialog . . .")
+	}
+
+	initDir = strings.Replace(initDir, "/", "\\", -1)
+
+	// Generate command
+	command := []string{"[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')",
+		"; $SaveChooser = New-Object System.Windows.Forms.SaveFileDialog -Property @{",
+		"Filter = '" + filter + "'",
+		"; InitialDirectory = '" + initDir + "'",
+		"}",
+		"; $SaveChooser.ShowDialog() | Out-Null",
+		"; Write-Output $SaveChooser.FileName",
+	}
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"command": command,
+			}),
+		).Debugln("Generate command")
+	}
+
+	// Generate message box
+	cmd, err = execute.Start(
+		true,
+		"powershell", command...,
+	)
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"command_object": cmd,
+				"error":          err,
+			}),
+		).Debugln("Generate message box")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return cmd, nil
+}
