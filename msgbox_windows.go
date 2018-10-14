dc13ed7827e9170ba4bb0262e926891c35dda12a
Leo Liu <karta1041337@gmail.com>
2018-10-14 16:15:17 +0800

    Use user32.dll in MsgBox

--- msgbox_windows.go
+++ msgbox_windows.go
@@ -1,176 +1,82 @@
 package dialog
 
 import (
+	"syscall"
+	"unsafe"
+
 	"github.com/sirupsen/logrus"
-	"leoliu.io/execute"
 	"leoliu.io/logger"
 )
 
+var (
+	MsgBoxBtnOk                uint = 0x0
+	MsgBoxBtnOkCancel          uint = 0x1
+	MsgBoxBtnAbortRetryIgnore  uint = 0x2
+	MsgBoxBtnYesNoCancel       uint = 0x3
+	MsgBoxBtnYesNo             uint = 0x4
+	MsgBoxBtnRetryCancel       uint = 0x5
+	MsgBoxBtnCancelTryContinue uint = 0x6
+
+	MsgBoxIconNone        uint = 0x00
+	MsgBoxIconError       uint = 0x10
+	MsgBoxIconQuestion    uint = 0x20
+	MsgBoxIconWarning     uint = 0x30
+	MsgBoxIconInformation uint = 0x40
+
+	MsgBoxDefaultButton1 uint = 0x000
+	MsgBoxDefaultButton2 uint = 0x100
+	MsgBoxDefaultButton3 uint = 0x200
+	MsgBoxDefaultButton4 uint = 0x300
+
+	MsgBoxTopMost uint = 0x41000
+
+	MsgBoxService uint = 0x200000
+
+	MsgBoxSelectOk       = 1
+	MsgBoxSelectCancel   = 2
+	MsgBoxSelectAbort    = 3
+	MsgBoxSelectRetry    = 4
+	MsgBoxSelectIgnore   = 5
+	MsgBoxSelectYes      = 6
+	MsgBoxSelectNo       = 7
+	MsgBoxSelectTry      = 10
+	MsgBoxSelectContinue = 11
+)
+
 // MsgBox create message box
-func MsgBox(title string, msg string, args ...string) (output string, err error) {
+func MsgBox(title string, msg string, flag uint) (int, error) {
 	if intLog {
 		intLogger.WithFields(
 			logger.DebugInfo(1, logrus.Fields{
-				"title":     title,
-				"message":   msg,
-				"arguments": args,
+				"title":   title,
+				"message": msg,
+				"flag":    flag,
 			}),
 		).Debugln("Create message box . . .")
 	}
 
-	// Create message box
-	cmd, err := BgMsgBox(title, msg, args...)
-	if err != nil {
-		if intLog {
-			intLogger.WithFields(logger.DebugInfo(1, logrus.Fields{})).
-				WithError(err).Errorln("Cannot create message box")
-		}
-		return
+	// Generate message box
+	if intLog {
+		intLogger.Debugln("Generate message box")
 	}
-	cmd.Wait()
-	output = cmd.Strout()
+	rtn, _, err := syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(msg))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
+		uintptr(flag),
+	)
+	if rtn == 0 {
+		intLogger.WithFields(logger.DebugInfo(1, logrus.Fields{})).
+			WithError(err).Errorln("Cannot generate message box")
+		return 0, err
+	}
 
 	if intLog {
 		intLogger.WithFields(
 			logger.DebugInfo(1, logrus.Fields{
-				"output": output,
+				"button_code": rtn,
 			}),
 		).Debugln("Create message box")
 	}
-	return
-}
-
-// BgMsgBox create message box in the background
-func BgMsgBox(title string, msg string, args ...string) (cmd *execute.Cmd, err error) {
-	if intLog {
-		intLogger.WithFields(
-			logger.DebugInfo(1, logrus.Fields{
-				"title":     title,
-				"message":   msg,
-				"arguments": args,
-			}),
-		).Debugln("Create message box in the background . . .")
-	}
-
-	// Parse arguments
-	btn := "0x0"
-	icon := "0x00"
-	defaultBtn := "0x00"
-	topMost := "false"
-	for _, val := range args {
-		switch val {
-		// Set button
-		case "Ok":
-			btn = "0x0"
-		case "O":
-			btn = "0x0"
-		case "OkCancel":
-			btn = "0x1"
-		case "OC":
-			btn = "0x1"
-		case "AbortRetryIgnore":
-			btn = "0x2"
-		case "ARI":
-			btn = "0x2"
-		case "YesNoCancel":
-			btn = "0x3"
-		case "YNC":
-			btn = "0x3"
-		case "YesNo":
-			btn = "0x4"
-		case "YN":
-			btn = "0x4"
-		case "RetryCancel":
-			btn = "0x5"
-		case "RC":
-			btn = "0x5"
-
-		// Set icon
-		case "None":
-			icon = "0x00"
-		case "N":
-			icon = "0x00"
-		case "Error":
-			icon = "0x10"
-		case "E":
-			icon = "0x10"
-		case "Question":
-			icon = "0x20"
-		case "Q":
-			icon = "0x20"
-		case "Warning":
-			icon = "0x30"
-		case "W":
-			icon = "0x30"
-		case "Information":
-			icon = "0x40"
-		case "I":
-			icon = "0x40"
-
-		// Set default button
-		case "DefaultButton1":
-			defaultBtn = "0x000"
-		case "DB1":
-			defaultBtn = "0x000"
-		case "DefaultButton2":
-			defaultBtn = "0x100"
-		case "DB2":
-			defaultBtn = "0x100"
-		case "DefaultButton3":
-			defaultBtn = "0x200"
-		case "DB3":
-			defaultBtn = "0x200"
-
-		// Set top most
-		case "TopMost":
-			topMost = "true"
-		case "TM":
-			topMost = "true"
-		}
-	}
-	if intLog {
-		intLogger.WithFields(
-			logger.DebugInfo(1, logrus.Fields{
-				"button":         btn,
-				"icon":           icon,
-				"default_button": defaultBtn,
-				"top_most":       topMost,
-			}),
-		).Debugln("Message box arguments")
-	}
-
-	// Generate command
-	command := []string{"[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')",
-		"; $FrmMain = New-Object 'System.Windows.Forms.Form'",
-		"; $FrmMain.TopMost = $" + topMost,
-		"; [System.Windows.Forms.MessageBox]::Show($FrmMain, '" + msg + "', '" + title + "', " + btn + ", " + icon + ", " + defaultBtn + ")",
-		"; $FrmMain.Dispose()",
-	}
-	if intLog {
-		intLogger.WithFields(
-			logger.DebugInfo(1, logrus.Fields{
-				"command": command,
-			}),
-		).Debugln("Generate command")
-	}
-
-	// Create message box in the background
-	cmd, err = execute.Start(
-		true,
-		"powershell", command...,
-	)
-	if err != nil {
-		intLogger.WithFields(logger.DebugInfo(1, logrus.Fields{})).
-			WithError(err).Errorln("Cannot create message box")
-		return nil, err
-	}
-
-	if intLog {
-		intLogger.WithFields(
-			logger.DebugInfo(1, logrus.Fields{
-				"command_object": cmd,
-			}),
-		).Debugln("Create message box in the background")
-	}
-	return cmd, nil
+	return int(rtn), nil
 }
